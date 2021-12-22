015bb2c1eb013808c15a04247c953421407e04b9
hoanglord <80758749+hoanglord@users.noreply.github.com>
2021-12-22 18:54:41 +0700

    Change-Id: I0000000000000000000000000000000000000000 hay
    Change-Id: I53c7a8e5da6e31b41204c3da5f78446391cd1f29

---
+++ 003float_price/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 22, 2021
+ *      Author: HOANG PC
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	double number = 45.183431231;
+	printf("so la = %0.14lf\n", number);
+	return 0;
+
+
+
+}
