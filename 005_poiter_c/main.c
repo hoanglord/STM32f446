22a3c14657a485b9d80f6bd871065be7dd0e925f
hoanglord <80758749+hoanglord@users.noreply.github.com>
2021-12-24 21:48:50 +0700

    con tro

---
+++ 005_poiter_c/main.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<stdint.h>
+
+long long int  g_data = 0xFFFEABCD11112345;
+
+int main(void)
+{
+
+	 int *pAddress = (int*)&g_data; // ep kieu giu lieu cho dia chi data
+
+
+	printf("value of pAddress %p\n",pAddress);
+	printf("Value at address %p is %x\n", pAddress, *pAddress);
+
+	pAddress = pAddress + 1;
+
+	printf("value of pAddress %p\n",pAddress);
+	printf("Value at address %p is %x", pAddress, *pAddress);
+
+
+
+}
